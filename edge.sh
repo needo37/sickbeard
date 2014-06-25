63e8c611ebd0ed728689a036bb73c7f8f71bca83
needo <malcolm@joker.lan>
2014-06-25 18:18:10 -0500

    phusion migration; edge enabled

---
+++ edge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Does the user want the latest version
+if [ -z "$EDGE" ]; then
+  echo "Bleeding edge not requested"
+else
+  apt-get install -qy git
+  rm -rf /opt/sickbeard
+  git clone https://github.com/midgetspy/Sick-Beard.git /opt/sickbeard
+  chown -R nobody:users /opt/sickbeard
+fi
